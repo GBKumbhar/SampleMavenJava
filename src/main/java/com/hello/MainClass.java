858ded88844432ab1e3fcfa21af1d1fdeae72d84
Ganesh <GBKumbhar@users.noreply.github.com>
2022-07-08 20:38:56 +0530

    sample java maven project

---
+++ src/main/java/com/hello/MainClass.java
@@ -0,0 +1,11 @@
+package com.hello;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello to you all");
+		
+	}
+
+}
---
+++ src/test/java/com/test/TestMain.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import org.junit.Test;
+
+public class TestMain {
+
+	@Test
+	public void test() {
+
+		System.out.println("Test Passes");
+	}
+
+	@Test
+	public void test2() {
+
+		System.out.println("Test 2 Passes");
+	}
+}
